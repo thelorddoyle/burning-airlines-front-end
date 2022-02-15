d6f97dfd549be793984fb6e2c75e300451d0181e
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-02-16 06:27:16 +1100

    Put button as Bootstrap component in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class Home extends React.Component {
     render() {
         return (
             <div>
                 <h1>HOME PAGE</h1>
+                <Button>Test button</Button>
             </div>
         );
     }
