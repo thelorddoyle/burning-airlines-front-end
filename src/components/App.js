06493e2054957180e99cb9830e4b5dfea079ebe4
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-02-16 10:41:43 +1100

    sorted components out

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,10 @@
 import '../App.css';
-import FlightsSearch from './FlightsSearch'
-import Planes from './Planes'
-import FlightResults from './FlightResults'
-
-import Home from './Home'
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
+import NavBar from './NavBar.js'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-
-      <h2>BURNING AIRLINES</h2>
-
-      <nav>
-
-      <Link to="/">My Account</Link> |&nbsp;
-      <Link to="/flights">Flight Search</Link>
-
-      <h3>GA Airlines</h3>
-
-      <Route exact path = '/' component={ Home } />
-      <Route exact path = '/flights' component={ FlightsSearch } />
-      <Route exact path = '/flights/:id' component={FlightResults} />
-      </nav>
-      </Router>
-      
+      <NavBar />
     </div>
   );
 }
