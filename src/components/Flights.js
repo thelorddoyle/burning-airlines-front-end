5adee3ac4a2fac299446ec1f32794dbabf636747
Alex King <calicoalix@gmail.com>
2022-02-15 21:40:45 +1100

    routes id works for planes and flights

--- src/components/Flights.js
+++ src/components/Flights.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 class Flights extends React.Component {
+
+    state = {
+        flightSelect: '',
+    }
+    handleInput = (ev) => {
+        this.setState({ flightSelect: ev.target.value });
+    }
+
+    handleSelection = (ev) => {
+        this.props.history.push(`/planes/${(this.state.flightSelect)}`)
+    }
+    
     render() {
         return (
             <div>
                 <h1>Flights</h1>
                 <div className="flights">
                     <p>Info about flights goes here</p>
+                    <form onSubmit={ this.handleSelection }>
+                    <input type="text" onChange={ this.handleInput } />
+                    </form>
                 </div>
             </div>
         );
--- src/components/Planes.js
+++ src/components/Planes.js
@@ -3,14 +3,14 @@ import React from 'react';
 class Planes extends React.Component {
 
     state = {
-        flightSelect: '',
+        planeSelect: '',
     }
     handleInput = (ev) => {
-        this.setState({ flightSelect: ev.target.value });
+        this.setState({ planeSelect: ev.target.value });
     }
 
     handleSelection = (ev) => {
-        this.props.history.push(`/planes/${(this.state.flightSelect)}`)
+        this.props.history.push(`/planes/${(this.state.planeSelect)}`)
     }
 
     render() {
@@ -21,7 +21,6 @@ class Planes extends React.Component {
                     <p>Info about planes goes here</p>
                     <form onSubmit={ this.handleSelection }>
                     <input type="text" onChange={ this.handleInput } />
-                    <button>Select</button>
                     </form>
                     {/* <button onClick={this.handleSelection(1)}>TEST ROUTE</button> */}
                 </div>
