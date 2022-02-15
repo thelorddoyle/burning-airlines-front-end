2e1527e7918bdc13e97a46606425cf4d8af0c0f3
Alex King <calicoalix@gmail.com>
2022-02-16 10:59:15 +1100

    no cors errors

--- src/components/FlightResults.js
+++ src/components/FlightResults.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import'../App.css'
+import FlightsSearch from './FlightsSearch'
+
+
+import axios from 'axios'
+
+const RAILS_SECRETS_BASE_URL ='http://localhost:3000/flights'
 
 class FlightResults extends Component {
 
@@ -10,24 +17,26 @@ class FlightResults extends Component {
 
     componentDidMount(){ 
         // this will call showFlightInfo() with: 
-        // this.showFlightInfo(this.props.match.params.id) 
+        // this.showFlightInfo(this.props.match.params.id)
+        this.showFlightInfo() 
+        
     }
 
-    showFlightInfo(flightNo){
+    showFlightInfo = async (flightNo) => {
         this.setState({ loading: true }); 
         // this will pull from backend and push into this.state.flightInfo array
 
-        // try {
-        //     const res = await axios.get( backend );
-        //     console.log('response', res.data);
-        //     this.setState({
-        //       flightInfo: res.data. ,
-        //       loading: false  
-        //     });
-        //   } catch( err ){
-        //      console.log('Error in search AJAX: ', err);
-        //      this.setState({ error: err, loading: false });
-        //   }
+        try {
+            const res = await axios.get( RAILS_SECRETS_BASE_URL );
+            console.log('response', res.data);
+            // this.setState({
+            //   flightInfo: res.dat
+            //   loading: false  
+            // });
+          } catch( err ){
+             console.log('Error in search AJAX: ', err);
+             this.setState({ error: err, loading: false });
+          }
 
     }
 
@@ -60,3 +69,6 @@ class FlightResults extends Component {
 }
 
 export default FlightResults;
+
+
+
