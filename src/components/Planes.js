03cbaa6df2c81644384e3a5ddab20a8bdf844f8d
Alex King <calicoalix@gmail.com>
2022-02-15 21:38:09 +1100

    route for flight ids works

--- src/components/Planes.js
+++ src/components/Planes.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
 class Planes extends React.Component {
+
+    state = {
+        flightSelect: '',
+    }
+    handleInput = (ev) => {
+        this.setState({ flightSelect: ev.target.value });
+    }
+
+    handleSelection = (ev) => {
+        this.props.history.push(`/planes/${(this.state.flightSelect)}`)
+    }
+
     render() {
         return (
             <div>
                 <h1>Planes</h1>
                 <div className="planes">
                     <p>Info about planes goes here</p>
+                    <form onSubmit={ this.handleSelection }>
+                    <input type="text" onChange={ this.handleInput } />
+                    <button>Select</button>
+                    </form>
+                    {/* <button onClick={this.handleSelection(1)}>TEST ROUTE</button> */}
                 </div>
             </div>
         );
